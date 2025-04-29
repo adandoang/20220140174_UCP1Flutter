0b7707f7482df0cc5a2482693eb1d50d3b69c734
adandoang <akhdanjzif@gmail.com>
2025-04-29 18:26:12 +0700

    membuat tampilan checklist

--- lib/hasilPendataan.dart
+++ lib/hasilPendataan.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class ResultPage extends StatelessWidget {
   final DateTime tanggal;
   final String jenisTransaksi;
@@ -17,4 +16,40 @@ class ResultPage extends StatelessWidget {
     required this.hargaSatuan,
     required this.totalHarga,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFBF9),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.green.withOpacity(0.1),
+              ),
+              padding: const EdgeInsets.all(20),
+              child: const Icon(
+                Icons.check,
+                color: Colors.green,
+                size: 60,
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Data Berhasil Disimpan',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 32),
+          ],
+        ),
+      ),
+    );
+  }
 }
