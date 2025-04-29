c8411fbf9e2b4a98bac40c6c3f332a07af7d6d83
adandoang <akhdanjzif@gmail.com>
2025-04-29 13:58:19 +0700

    membuat datapiket state dan fungsi untuk tambah tugas

--- lib/dataPiket.dart
+++ lib/dataPiket.dart
@@ -6,3 +6,18 @@ class DataPiketPage extends StatefulWidget {
   @override
   State<DataPiketPage> createState() => _DataPiketPageState();
 }
+
+class _DataPiketPageState extends State<DataPiketPage> {
+  final TextEditingController _tugasController = TextEditingController();
+  List<String> _daftarTugas = [];
+
+  void _tambahTugas() {
+    final tugas = _tugasController.text;
+    if (tugas.isNotEmpty) {
+      setState(() {
+        _daftarTugas.add(tugas);
+        _tugasController.clear();
+      });
+    }
+  }
+}
