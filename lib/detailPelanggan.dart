f832fe3cede492ddce7c34cb26c2c8585b91ef11
adandoang <akhdanjzif@gmail.com>
2025-04-29 16:38:32 +0700

    membuat isi tampilan di detail pelanggan

--- lib/detailPelanggan.dart
+++ lib/detailPelanggan.dart
@@ -19,4 +19,81 @@ class DetailPelangganPage extends StatelessWidget {
     required this.provinsi,
     required this.kodePos,
   }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Color(0xFFFF3D00);
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFDF4F2),
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: Text('Detail $nama'),
+        centerTitle: true,
+        leading: BackButton(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const CircleAvatar(
+              radius: 40,
+              backgroundImage: AssetImage('assets/images/profile.jpg'),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              nama,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Text(email),
+            Text(noHp),
+            const SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Alamat', style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+            const SizedBox(height: 5),
+            TextFormField(
+              initialValue: alamat,
+              readOnly: true,
+             
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Provinsi', style: TextStyle(fontWeight: FontWeight.bold)),
+                      const SizedBox(height: 5),
+                      TextFormField(
+                        initialValue: provinsi,
+                        readOnly: true,
+                        
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Kode Pos', style: TextStyle(fontWeight: FontWeight.bold)),
+                      const SizedBox(height: 5),
+                      TextFormField(
+                        initialValue: kodePos,
+                        readOnly: true,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+      )
+    );
+  }
 }
